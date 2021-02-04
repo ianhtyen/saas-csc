4da78b9620c69321151d7b0aec35997f644a2dec
minxianlim <limminxian316@gmail.com>
2021-02-05 01:20:51 +0800

    envconfig

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": "ASIA6COLRGFUN7WXBVIR", 
+    "secretAccessKey": "cdaJ8zNU0lOLJ0q1ogqbQ54t3G2B2kIoUbXI4SBt",
+    "sessionToken": "FwoGZXIvYXdzEDIaDDFzAJQshrme90uUbiLLAfjeQIQt4lVULDq1zfAKjbyEOQ/Fs4Qfqh9muoxtQZTgLXbSS6irZD7Y9CK7SryxYPsZBUThzuuiwCbPVPuV4NsA7NX7hTTbfp5BNfLAMcfdeWg5mLe+6u324+FS0X6gqWgTXp7lNt/SpjGL5NDWROQBT/UqTviL1xRhIZKhHFr/0MGY8PrUZlVZ9amphxqZKmNuBqyiJebZ7jF8CcBuz2+9+K93ifq603Lz4w+Sztt4e5RkCdXYtdwSi/5Gm1YSWLU/jjdrploRhlloKL7F8IAGMi2zPtqj6JPaO4kHAJjXSFcXFXtAUSegdxhNFdeMbHB3g/1f4LhuI1Ur8KluYJo="
+};
