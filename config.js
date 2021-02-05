2fe413c6fe1ca8d0d29a95f95985cb5a71c31d62
minxianlim <56817531+minxianlim@users.noreply.github.com>
2021-02-05 14:49:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.awsConfig = {
     "region": "us-east-1",
     "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
-    "accessKeyId": "ASIA6COLRGFUN7WXBVIR", 
-    "secretAccessKey": "cdaJ8zNU0lOLJ0q1ogqbQ54t3G2B2kIoUbXI4SBt",
-    "sessionToken": "FwoGZXIvYXdzEDIaDDFzAJQshrme90uUbiLLAfjeQIQt4lVULDq1zfAKjbyEOQ/Fs4Qfqh9muoxtQZTgLXbSS6irZD7Y9CK7SryxYPsZBUThzuuiwCbPVPuV4NsA7NX7hTTbfp5BNfLAMcfdeWg5mLe+6u324+FS0X6gqWgTXp7lNt/SpjGL5NDWROQBT/UqTviL1xRhIZKhHFr/0MGY8PrUZlVZ9amphxqZKmNuBqyiJebZ7jF8CcBuz2+9+K93ifq603Lz4w+Sztt4e5RkCdXYtdwSi/5Gm1YSWLU/jjdrploRhlloKL7F8IAGMi2zPtqj6JPaO4kHAJjXSFcXFXtAUSegdxhNFdeMbHB3g/1f4LhuI1Ur8KluYJo="
+    "accessKeyId": "ASIA6COLRGFUJOXKYSSG", 
+    "secretAccessKey": "NYR5ITC6JpenDqndfUlwXmbL9k9YXJqg+gPDzTVJ",
+    "sessionToken": "FwoGZXIvYXdzEEAaDMUMraeRl+LnJBDofyLLASwioOWzsjl2uvCPlwH8BAPQkl65w9lukHDA00nbsfUU8UXEvusFTqU+Irp6Rcznbtj2cZelfAgdkIc5r74e+6EVc5LE8PlUq1+Q/FDvl3Sudvqo5KpATvil2gtQXP29Xh+5LxznVBs0av3Z2wjdJ7DXe7mFVdsLqfG2qB0xpqloO+HGFA8t/fuziJ3PDFI9bU1FGhYjlSa+dBHm9uV25LC2nSep+MzB4gDrjvWnN+x06C7XE9v70zZ6SD8XI7ZGjycDT4giBlcodkWzKMbN84AGMi2Pp1/YambaXSHxRCtfOoC/c9upsnujh6TdSnGivYBS+fQ2EQ9feWDdrjGPLdA="
 };
