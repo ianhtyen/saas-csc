4a18d32b94caf3cf5e5b6821c5103b4175755c54
Ian Hong <49706709+ianhtyen@users.noreply.github.com>
2021-02-10 22:03:55 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-exports.awsConfig = {
-    "region": "us-east-1",
-    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
-    "accessKeyId": "ASIA6COLRGFUBHOXX65K", 
-    "secretAccessKey": "18E+4p0hq/lP8Za/NsbNHdZJKVxZesM6KMYUfB5A",
-    "sessionToken": "FwoGZXIvYXdzEGQaDAulEwjFl9OsnHGo6iLLATjGv2F3d05iobliLkVNgwsIdafVxHig7xskxowtY7TiyFO5MCMWgk80whimt5eTSFwCwRuzfypGBl7X3Rfw2Th/T/3+q6Uy5MdT7WUBLj0MOKjdvIETCCxz8zQEAK/eopRgkMqtfnkGZgyFdkKr+d1vjzVDw8bzKuWiM2gTdYm0yMtQeFBQhkmk+lO6N0QHaI9lKdPv+b/ev2U+sqo17fWe2JghwkmQTgu6nEfJy0Xyzh4RcOYNm9VQyz0NBCrXwkQZkol1JDXz35sVKMXF+4AGMi3SbRzwVTncqGSaXK0al7FrvABJa5O/lQwx9tG/W7stqB16DROrwLW/q3UgsWU="
-};
